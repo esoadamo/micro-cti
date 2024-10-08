76c384d524f5ec46f573e84355dd6c81525495b9
Adam Hlavacek <git@adamhlavacek.com>
2024-10-08 18:10:23 +0200

    feat: fetch airtable posts

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import asyncio
 
-from posts import generate_tags, get_mastodon_posts, generate_random_color
+from posts import generate_tags, get_mastodon_posts, get_airtable_posts
 
 
 async def main() -> None:
+    print('[*] Fetching started')
+    async for _ in get_airtable_posts():
+        pass
+    print('[*] Airtable fetched')
     async for _ in get_mastodon_posts():
         pass
+    print('[*] Mastodon fetched')
     await generate_tags()
+    print('[*] Tags generated')
 
 
 if __name__ == "__main__":
