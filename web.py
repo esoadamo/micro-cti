ef91456d4c91070daa6fd89fa95bbb451c9b74f8
Adam Hlavacek <git@adamhlavacek.com>
2024-11-04 08:56:34 +0100

    feat(ioc): parse iocs

--- web.py
+++ web.py
@@ -1,5 +1,8 @@
 import time
+from datetime import datetime
 from email.utils import format_datetime as format_rfc2822
+from typing import List, Optional
+from typing_extensions import TypedDict
 
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, HTMLResponse, PlainTextResponse
@@ -8,11 +11,18 @@ from lark import ParseError
 # noinspection PyPackageRequirements,PyProtectedMember
 from starlette.templating import _TemplateResponse
 
+from ioc import search_iocs, IoCLink
 from posts import get_latest_ingestion_time
 from search import search_posts
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+
+class IoCSearchResponse(TypedDict):
+    search_term: str
+    iocs: List[IoCLink]
+    latest_ingestion_time: Optional[datetime]
 
 
 def render_template(filename, request, headers=None, **context):
@@ -39,15 +49,26 @@ async def app_search(request: Request, q: str = "") -> _TemplateResponse:
 
     time_delta_ms = int(1000 * (time.time() - time_start))
     return render_template(
-        'search.html',
+        'search_posts.html',
         request,
-        posts=posts,
+        results=posts,
         search_term=search_term,
         latest_ingestion_time=await get_latest_ingestion_time(),
         error=error,
         time_render=time_delta_ms,
         search_count=search_back_data.get('cnt_search', 0)
     )
+
+
+@app.get("/ioc/search/")
+async def app_search(q: str) -> IoCSearchResponse:
+    search_term = q
+    iocs = await search_iocs(search_term)
+    return {
+        'search_term': search_term,
+        'iocs': iocs,
+        'latest_ingestion_time': await get_latest_ingestion_time(),
+    }
 
 
 @app.get("/rss/", response_class=PlainTextResponse)
