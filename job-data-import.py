b2e2ff79788e199563e9019f9ea7cac36be9de28
Adam Hlaváček <git@adamhlavacek.com>
2024-11-28 23:14:13 +0100

    feat(data): import/export

---
+++ job-data-import.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import asyncio
+
+from prisma.models import Post
+
+from db import get_db
+
+
+async def main() -> None:
+    print('[*] Process started')
+    db = await get_db()
+    print('[*] Database connected')
+    file_backup = Path('posts.jsonl')
+
+    with file_backup.open('r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            post = Post.model_validate_json(line)
+            print(f'[*] Post id: {post.id}', flush=True)
+            data = post.model_dump()
+            del data['id']
+            tags = data['tags']
+            del data['tags']
+            del data['iocs']
+            post = await db.post.create(data=data)
+            if tags:
+                for tag in tags:
+                    del tag['id']
+                    del tag['posts']
+                    tag = await db.tag.upsert(where={'name': tag['name']}, data={'create': tag, 'update': {}})
+                    await db.post.update(where={'id': post.id}, data={'tags': {'connect': [{'id': tag.id}]}})
+
+    print(f'[*] Restored data from {file_backup}')
+    await db.disconnect()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
