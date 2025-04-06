0ead13da7d4d1482506e2e032087e32773e56703
Adam Hlavacek <git@adamhlavacek.com>
2025-04-06 10:33:45 +0200

    fix(post): rss missing attributes

--- posts.py
+++ posts.py
@@ -113,32 +113,42 @@ async def get_rss_posts() -> AsyncIterable[Post]:
     for feed in feeds:
         # noinspection PyBroadException
         try:
-            time.sleep(1)
+            time.sleep(10)
             source = feed['name']
+            date_now = datetime.now(tz=timezone.utc)
 
             max_post = await (await get_db()).post.find_first(where={'source': source}, order={'created_at': 'desc'})
             min_post_time = max_post.created_at if max_post else datetime.now(tz=timezone.utc) - timedelta(days=1)
 
-            for rss_post in feedparser.parse(feed['url']).entries:
+            for rss_post in feedparser.parse(
+                feed['url'],
+                agent=f'RSS Reader {date_now.year}.{date_now.month}'
+            ).entries:
                 try:
                     created_at = datetime.fromisoformat(rss_post.published)
                 except ValueError:
                     created_at = parsedate_to_datetime(rss_post.published)
                     assert created_at is not None
+                except AttributeError:
+                    continue
 
                 if created_at < min_post_time:
                     continue
 
-                source_id = rss_post.link
-                url = rss_post.link
-                content_html = rss_post.title + " " + rss_post.summary
+                try:
+                    author = rss_post.author
+                    source_id = rss_post.link
+                    url = rss_post.link
+                    content_html = rss_post.title + " " + rss_post.summary
+                except AttributeError:
+                    continue
                 content_txt = read_html(content_html)
                 db = await get_db()
-                if  len(content_txt.split()) > 3 and not await db.post.find_first(where={'source': source, 'source_id': source_id}):
+                if len(content_txt.split()) > 3 and not await db.post.find_first(where={'source': source, 'source_id': source_id}):
                     post = await db.post.create({
                         'source': source,
                         'source_id': source_id,
-                        'user': rss_post.author,
+                        'user': author,
                         'url': url,
                         'created_at': created_at,
                         'fetched_at': datetime.now(tz=timezone.utc),
